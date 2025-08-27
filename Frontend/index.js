ec700de6b4dbc65711ea01f32357655a08606e10
Renan <renan.guilherme.ballock@gmail.com>
2025-08-27 14:03:34 -0300

    atualização

---
+++ Frontend/index.js
@@ -0,0 +1,16 @@
+fetch("http://localhost:3000/usuarios").then(res=>{
+    if(!res.ok){
+        throw new Error("Erro ao buscar usuarios");
+    }
+
+    return res.json();
+}).then(usuarios=>{
+    const listaUsuarios = document.getElementById("lista-usuarios");
+    usuarios.forEach(usuario => {
+        console.log(usuario.nome);
+        listaUsuarios.innerHTML += `<li class="list-group-item">${usuario.nome}</li>`;
+    });
+})
+.catch(err=>{
+    console.error(err);
+});
